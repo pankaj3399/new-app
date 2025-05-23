1c154af3a9e653c203761d396e0b65300d37c335
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2025-05-24 02:47:45 +0530

    Try fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        ".prisma/client": "commonjs .prisma/client",
+        "@prisma/client": "commonjs @prisma/client",
+      });
+    }
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
